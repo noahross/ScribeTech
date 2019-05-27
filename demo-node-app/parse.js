83f1053a327474c1ae5d7ba2cd5f836e3b3e174b
Noah <it@noah-ross.com>
2019-05-27 16:21:51 -0700

    Adds contractions and better contraction processing

--- demo-node-app/parse.js
+++ demo-node-app/parse.js
@@ -1,8 +1,31 @@
 var fs = require('fs');
+var path = require('path');
 
-try {  
-    var data = fs.readFileSync('~\dictioanries\English (USA).txt', 'utf8');
-    console.log(data);    
-} catch(e) {
-    console.log('Error:', e.stack);
+//res.send('Working on it.');
+var filePath = path.join(__dirname, 'dictionaries', 'English (USA).txt');
+var wordList = fs.readFileSync(filePath, 'utf8').split(',');
+console.log(wordList.length);
+
+var textFilePath = path.join(__dirname, 'The Book of Six.txt');
+var bodyText = fs.readFileSync(textFilePath, 'utf8');
+var words = bodyText.trim().replace(/(?=[\W_])(?:[^\w]+_*|_+)+/g, " ").split(" ").sort();
+var wordCount = 0;
+var wordMap = {};
+
+words.forEach(function(word){
+    word = word.toLowerCase();
+    if(word.length > 1 && !wordList.includes(word)) {
+        if(wordMap[word] === null || wordMap[word] === undefined) {
+            wordMap[word] = 1;
+            wordCount++;
+        } else {
+            wordMap[word] = wordMap[word] + 1;
+        }
+    }
+});
+
+var htmlBody = wordList.length.toString() + ' - ' + words.length.toString() + ' - ' + wordCount;
+for(var key in wordMap) {
+    htmlBody += '<br />' + key + ' - ' + wordMap[key].toString() + ' instances'; 
 }
+return htmlBody;
--- demo-node-app/routes/index.js
+++ demo-node-app/routes/index.js
@@ -53,18 +53,19 @@ router.get('/parse', (req, res) => {
     var fs = require('fs');
     var path = require('path');
 
+    //res.send('Working on it.');
     var filePath = path.join(__dirname, '..', 'dictionaries', 'English (USA).txt');
     var wordList = fs.readFileSync(filePath, 'utf8').split(',');
     console.log(wordList.length);
 
     var textFilePath = path.join(__dirname, '..', 'The Book of Six.txt');
     var bodyText = fs.readFileSync(textFilePath, 'utf8');
-    var words = bodyText.trim().replace(/(?=[\W_])(?:[^\w]+_*|_+)+/g, " ").split(" ").sort();
+    var words = bodyText.trim().replace(/[\u2018\u2019]/g, '\'').replace(/(?=[\W_])(?:[^\w']+_*|_+)+/g, " ").toLowerCase().split(" ").sort();
     var wordCount = 0;
     var wordMap = {};
 
     words.forEach(function(word){
-        word = word.toLowerCase();
+        word = processWord(word);
         if(word.length > 1 && !wordList.includes(word)) {
             if(wordMap[word] === null || wordMap[word] === undefined) {
                 wordMap[word] = 1;
@@ -75,11 +76,72 @@ router.get('/parse', (req, res) => {
         }
     });
 
-    var htmlBody = wordList.length.toString() + ' - ' + words.length.toString() + ' - ' + wordCount;
+    var htmlBody = wordList.length.toString() + ' - ' + words.length.toString() + ' - ' + wordCount + '</p>';
+    htmlBody += '<table><tr><th>Word</th><th>Instances</th></tr>';
     for(var key in wordMap) {
-        htmlBody += '<br />' + key + ' - ' + wordMap[key].toString() + ' instances'; 
+        htmlBody += '<tr><td>' + key + '</td><td>' + wordMap[key].toString() + '</td></tr>';
     }
     res.send(htmlBody);
 });
+
+router.get('/extractContractions', (req, res) => {
+    var fs = require('fs');
+    var path = require('path');
+
+    //res.send('Working on it.');
+    var filePath = path.join(__dirname, '..', 'dictionaries', 'English (USA).txt');
+    var wordList = fs.readFileSync(filePath, 'utf8').split(',');
+    console.log(wordList.length);
+
+    var textFilePath = path.join(__dirname, '..', 'The Book of Six.txt');
+    var bodyText = fs.readFileSync(textFilePath, 'utf8');
+    var words = bodyText.trim().replace(/[\u2018\u2019]/g, '\'').replace(/(?=[\W_])(?:[^\w']+_*|_+)+/g, " ").toLowerCase().split(" ").sort();
+    var wordCount = 0;
+    var wordMap = {};
+    var htmlBody = '';
+
+    words.forEach(function(word){
+        if(word.includes('\'')) {
+            var ind = word.indexOf('\'');
+            if(ind === 0 || ind === word.length-1 || (ind === word.length-2 && word[word.length-1] === 's')) {
+                word = word.substring(0,word.length-2);
+            } else if(ind >= word.length-3){
+                wordMap[word] = 1;
+            }
+        }
+    });
+
+    var htmlBody = '';
+    for(var key in wordMap) {
+        if(htmlBody.length != 0) {
+            htmlBody += ',';
+        }
+        htmlBody += key;
+    }
+    res.send(htmlBody);
+});
+
+var processWord = function(word) {
+    if(word.includes('\'')) {
+        var ind = word.indexOf('\'');
+        if(ind === word.length-1 || (ind === word.length-2 && word[word.length-1] === 's')) {
+            word = word.substring(0,word.length-2);
+        } else if(ind === 0){
+            word = replaceAll(word, '\'', '');
+        } else {
+            console.log(word);
+        }
+    }
+
+    return word;
+};
+
+var replaceAll = function(str, find, replace) {
+    return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
+};
+
+var escapeRegExp = function(str) {
+    return str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+};
 
 module.exports = router;
